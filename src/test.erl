26ddba09b0a0ed98ca08b598042da5eeed5e182f
rajan modi <rajanmodi30@gmail.com>
2019-09-23 17:55:52 +0530

    first commit

--- src/test.erl
+++ src/test.erl
@@ -1,291 +1,124 @@
 -module(test).
-
 -include_lib("emqx/include/emqx.hrl").
 
--define(APP, test).
-
--export([ register_metrics/0
-        , load/0
+-export([ 
+    load/1
         , unload/0
         ]).
 
--export([ on_client_connected/4
+%% Hooks functions
+-export([ on_client_authenticate/2
+        , on_client_check_acl/5
+        , on_client_connected/4
         , on_client_disconnected/3
-        ]).
-
--export([ on_client_subscribe/3
-        , on_client_unsubscribe/3
-        ]).
-
--export([ on_session_created/3
+        , on_client_subscribe/4
+        , on_client_unsubscribe/4
+        , on_session_created/3
+        , on_session_resumed/3
+        , on_session_terminated/3
         , on_session_subscribed/4
         , on_session_unsubscribed/4
-        , on_session_terminated/3
-        ]).
-
--export([ on_message_publish/2
+        , on_message_publish/2
         , on_message_deliver/3
         , on_message_acked/3
+        , on_message_dropped/3
         ]).
 
--define(LOG(Level, Format, Args), emqx_logger:Level("test: " ++ Format, Args)).
+%% Called when the plugin application start
+load(Env) ->
+    emqx:hook('client.authenticate', fun ?MODULE:on_client_authenticate/2, [Env]),
+    emqx:hook('client.check_acl', fun ?MODULE:on_client_check_acl/5, [Env]),
+    emqx:hook('client.connected', fun ?MODULE:on_client_connected/4, [Env]),
+    emqx:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3, [Env]),
+    emqx:hook('client.subscribe', fun ?MODULE:on_client_subscribe/4, [Env]),
+    emqx:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4, [Env]),
+    emqx:hook('session.created', fun ?MODULE:on_session_created/3, [Env]),
+    emqx:hook('session.resumed', fun ?MODULE:on_session_resumed/3, [Env]),
+    emqx:hook('session.subscribed', fun ?MODULE:on_session_subscribed/4, [Env]),
+    emqx:hook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4, [Env]),
+    emqx:hook('session.terminated', fun ?MODULE:on_session_terminated/3, [Env]),
+    emqx:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
+    emqx:hook('message.deliver', fun ?MODULE:on_message_deliver/3, [Env]),
+    emqx:hook('message.acked', fun ?MODULE:on_message_acked/3, [Env]),
+    emqx:hook('message.dropped', fun ?MODULE:on_message_dropped/3, [Env]).
 
-register_metrics() ->
-    [emqx_metrics:new(MetricName) || MetricName <- ['test.client_connected',
-                                                    'test.client_disconnected',
-                                                    'test.client_subscribe',
-                                                    'test.client_unsubscribe',
-                                                    'test.session_created',
-                                                    'test.session_subscribed',
-                                                    'test.session_unsubscribed',
-                                                    'test.session_terminated',
-                                                    'test.message_publish',
-                                                    'test.message_deliver',
-                                                    'test.message_acked']].
+on_client_authenticate(Credentials = #{client_id := ClientId, password := Password}, _Env) ->
+    io:format("Client(~s) authenticate, Password:~p ~n", [ClientId, Password]),
+    {stop, Credentials#{auth_result => success}}.
 
-load() ->
-    lists:foreach(
-      fun({Hook, Fun, Filter}) ->
-          load_(Hook, binary_to_atom(Fun, utf8), {Filter})
-      end, parse_rule(application:get_env(?APP, rules, []))).
+on_client_check_acl(#{client_id := ClientId}, PubSub, Topic, DefaultACLResult, _Env) ->
+    io:format("Client(~s) authenticate, PubSub:~p, Topic:~p, DefaultACLResult:~p~n",
+             [ClientId, PubSub, Topic, DefaultACLResult]),
+    {stop, allow}.
 
-unload() ->
-    lists:foreach(
-      fun({Hook, Fun, _Filter}) ->
-          unload_(Hook, binary_to_atom(Fun, utf8))
-      end, parse_rule(application:get_env(?APP, rules, []))).
+on_client_connected(#{client_id := ClientId}, ConnAck, ConnAttrs, _Env) ->
+    io:format("Client(~s) connected, connack: ~w, conn_attrs:~p~n", [ClientId, ConnAck, ConnAttrs]).
 
+on_client_disconnected(#{client_id := ClientId}, ReasonCode, _Env) ->
+    io:format("Client(~s) disconnected, reason_code: ~w~n", [ClientId, ReasonCode]).
 
-%%--------------------------------------------------------------------
-%% Client connected
-%%--------------------------------------------------------------------
-on_client_connected(#{client_id := ClientId, username := Username}, 0, ConnInfo, _Env) ->
-    emqx_metrics:inc('web_hook.client_connected'),
-    %% Code Start
+on_client_subscribe(#{client_id := ClientId}, _Properties, RawTopicFilters, _Env) ->
+    io:format("Client(~s) will subscribe: ~p~n", [ClientId, RawTopicFilters]),
+    {ok, RawTopicFilters}.
 
-    %% Here is the code
+on_client_unsubscribe(#{client_id := ClientId}, _Properties, RawTopicFilters, _Env) ->
+    io:format("Client(~s) unsubscribe ~p~n", [ClientId, RawTopicFilters]),
+    {ok, RawTopicFilters}.
 
-    %% End
-    ok;
+on_session_created(#{client_id := ClientId}, SessAttrs, _Env) ->
+    io:format("Session(~s) created: ~p~n", [ClientId, SessAttrs]).
 
-on_client_connected(#{}, _ConnAck, _ConnInfo, _Env) ->
-    ok.
+on_session_resumed(#{client_id := ClientId}, SessAttrs, _Env) ->
+    io:format("Session(~s) resumed: ~p~n", [ClientId, SessAttrs]).
 
-%%--------------------------------------------------------------------
-%% Client disconnected
-%%--------------------------------------------------------------------
-on_client_disconnected(#{}, auth_failure, _Env) ->
-    ok;
-on_client_disconnected(Client, {shutdown, Reason}, Env) when is_atom(Reason) ->
-    on_client_disconnected(Reason, Client, Env);
-on_client_disconnected(#{client_id := ClientId, username := Username}, Reason, _Env)
-    when is_atom(Reason) ->
-    emqx_metrics:inc('web_hook.client_disconnected'),
-    %% Code Start
+on_session_subscribed(#{client_id := ClientId}, Topic, SubOpts, _Env) ->
+    io:format("Session(~s) subscribe ~s with subopts: ~p~n", [ClientId, Topic, SubOpts]).
 
-    %% Here is the code
+on_session_unsubscribed(#{client_id := ClientId}, Topic, Opts, _Env) ->
+    io:format("Session(~s) unsubscribe ~s with opts: ~p~n", [ClientId, Topic, Opts]).
 
-    %% End
-    ok;
-on_client_disconnected(_, Reason, _Env) ->
-    ?LOG(error, "Client disconnected, cannot encode reason: ~p", [Reason]),
-    ok.
+on_session_terminated(#{client_id := ClientId}, ReasonCode, _Env) ->
+    io:format("Session(~s) terminated: ~p.", [ClientId, ReasonCode]).
 
-%%--------------------------------------------------------------------
-%% Client subscribe
-%%--------------------------------------------------------------------
-on_client_subscribe(#{client_id := ClientId, username := Username}, TopicTable, {Filter}) ->
-    lists:foreach(fun({Topic, Opts}) ->
-      with_filter(
-        fun() ->
-          emqx_metrics:inc('web_hook.client_subscribe'),
-          %% Code Start
-
-          %% Here is the code
-
-          %% End
-        end, Topic, Filter)
-    end, TopicTable).
-
-%%--------------------------------------------------------------------
-%% Client unsubscribe
-%%--------------------------------------------------------------------
-on_client_unsubscribe(#{client_id := ClientId, username := Username}, TopicTable, {Filter}) ->
-    lists:foreach(fun({Topic, Opts}) ->
-      with_filter(
-        fun() ->
-          emqx_metrics:inc('web_hook.client_unsubscribe'),
-          %% Code Start
-
-          %% Here is the code
-
-          %% End
-        end, Topic, Filter)
-    end, TopicTable).
-
-%%--------------------------------------------------------------------
-%% Session created
-%%--------------------------------------------------------------------
-on_session_created(#{client_id := ClientId}, SessInfo, _Env) ->
-    emqx_metrics:inc('web_hook.session_created'),
-    %% Code Start
-
-    %% Here is the code
-
-    %% End
-    ok.
-
-%%--------------------------------------------------------------------
-%% Session subscribed
-%%--------------------------------------------------------------------
-on_session_subscribed(#{client_id := ClientId}, Topic, Opts, {Filter}) ->
-    with_filter(
-      fun() ->
-        emqx_metrics:inc('web_hook.session_subscribed'),
-        %% Code Start
-
-        %% Here is the code
-
-        %% End
-      end, Topic, Filter).
-
-%%--------------------------------------------------------------------
-%% Session unsubscribed
-%%--------------------------------------------------------------------
-on_session_unsubscribed(#{client_id := ClientId}, Topic, _Opts, {Filter}) ->
-    with_filter(
-      fun() ->
-        emqx_metrics:inc('web_hook.session_unsubscribed'),
-        %% Code Start
-
-        %% Here is the code
-
-        %% End
-      end, Topic, Filter).
-
-%%--------------------------------------------------------------------
-%% Session terminated
-%%--------------------------------------------------------------------
-on_session_terminated(Info, {shutdown, Reason}, Env) when is_atom(Reason) ->
-    on_session_terminated(Info, Reason, Env);
-on_session_terminated(#{client_id := ClientId}, Reason, _Env) when is_atom(Reason) ->
-    emqx_metrics:inc('web_hook.session_terminated'),
-    %% Code Start
-
-    %% Here is the code
-
-    %% End
-    ok;
-on_session_terminated(#{}, Reason, _Env) ->
-    ?LOG(error, "Session terminated, cannot encode the reason: ~p", [Reason]),
-    ok.
-
-%%--------------------------------------------------------------------
-%% Message publish
-%%--------------------------------------------------------------------
+%% Transform message and return
 on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
     {ok, Message};
-on_message_publish(Message = #message{topic = Topic, flags = #{retain := Retain}}, {Filter}) ->
-    with_filter(
-      fun() ->
-        emqx_metrics:inc('web_hook.message_publish'),
-        %% Code Start
 
-        %% Here is the code
+on_message_publish(Message, _Env) ->
+    io:format("Publish ~s~n", [emqx_message:format(Message)]),
+    io:fwrite("Hello world Message Published!~n", []),
+    {ok, Message}.
 
-        %% End
-        {ok, Message}
-      end, Message, Topic, Filter).
+on_message_deliver(#{client_id := ClientId}, Message, _Env) ->
+    io:format("Deliver message to client(~s): ~s~n", [ClientId, emqx_message:format(Message)]),
+    {ok, Message}.
 
-%%--------------------------------------------------------------------
-%% Message deliver
-%%--------------------------------------------------------------------
-on_message_deliver(#{client_id := ClientId, username := Username},
-                   Message = #message{topic = Topic, flags = #{retain := Retain}},
-                   {Filter}) ->
-  with_filter(
-    fun() ->
-      emqx_metrics:inc('web_hook.message_deliver'),
-      %% Code Start
+on_message_acked(#{client_id := ClientId}, Message, _Env) ->
+    io:format("Session(~s) acked message: ~s~n", [ClientId, emqx_message:format(Message)]),
+    {ok, Message}.
 
-      %% Here is the code
+on_message_dropped(_By, #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
+    ok;
+on_message_dropped(#{node := Node}, Message, _Env) ->
+    io:format("Message dropped by node ~s: ~s~n", [Node, emqx_message:format(Message)]);
+on_message_dropped(#{client_id := ClientId}, Message, _Env) ->
+    io:format("Message dropped by client ~s: ~s~n", [ClientId, emqx_message:format(Message)]).
 
-      %% End
-    end, Topic, Filter).
-
-%%--------------------------------------------------------------------
-%% Message acked
-%%--------------------------------------------------------------------
-on_message_acked(#{client_id := ClientId}, Message = #message{topic = Topic, flags = #{retain := Retain}}, {Filter}) ->
-    with_filter(
-      fun() ->
-        emqx_metrics:inc('web_hook.message_acked'),
-        %% Code Start
-
-        %% Here is the code
-
-        %% End
-      end, Topic, Filter).
-
-%%--------------------------------------------------------------------
-%% Internal functions
-%%--------------------------------------------------------------------
-parse_rule(Rules) ->
-    parse_rule(Rules, []).
-parse_rule([], Acc) ->
-    lists:reverse(Acc);
-parse_rule([{Rule, Conf} | Rules], Acc) ->
-    Params = jsx:decode(iolist_to_binary(Conf)),
-    Action = proplists:get_value(<<"action">>, Params),
-    Filter = proplists:get_value(<<"topic">>, Params),
-    parse_rule(Rules, [{list_to_atom(Rule), Action, Filter} | Acc]).
-
-with_filter(Fun, _, undefined) ->
-    Fun(), ok;
-with_filter(Fun, Topic, Filter) ->
-    case emqx_topic:match(Topic, Filter) of
-        true  -> Fun(), ok;
-        false -> ok
-    end.
-
-with_filter(Fun, _, _, undefined) ->
-    Fun();
-with_filter(Fun, Msg, Topic, Filter) ->
-    case emqx_topic:match(Topic, Filter) of
-        true  -> Fun();
-        false -> {ok, Msg}
-    end.
-
-a2b(A) when is_atom(A) -> erlang:atom_to_binary(A, utf8);
-a2b(A) -> A.
-
-load_(Hook, Fun, Params) ->
-    case Hook of
-        'client.connected'    -> emqx:hook(Hook, fun ?MODULE:Fun/4, [Params]);
-        'client.disconnected' -> emqx:hook(Hook, fun ?MODULE:Fun/3, [Params]);
-        'client.subscribe'    -> emqx:hook(Hook, fun ?MODULE:Fun/3, [Params]);
-        'client.unsubscribe'  -> emqx:hook(Hook, fun ?MODULE:Fun/3, [Params]);
-        'session.created'     -> emqx:hook(Hook, fun ?MODULE:Fun/3, [Params]);
-        'session.subscribed'  -> emqx:hook(Hook, fun ?MODULE:Fun/4, [Params]);
-        'session.unsubscribed'-> emqx:hook(Hook, fun ?MODULE:Fun/4, [Params]);
-        'session.terminated'  -> emqx:hook(Hook, fun ?MODULE:Fun/3, [Params]);
-        'message.publish'     -> emqx:hook(Hook, fun ?MODULE:Fun/2, [Params]);
-        'message.acked'       -> emqx:hook(Hook, fun ?MODULE:Fun/3, [Params]);
-        'message.deliver'     -> emqx:hook(Hook, fun ?MODULE:Fun/3, [Params])
-    end.
-
-unload_(Hook, Fun) ->
-    case Hook of
-        'client.connected'    -> emqx:unhook(Hook, fun ?MODULE:Fun/4);
-        'client.disconnected' -> emqx:unhook(Hook, fun ?MODULE:Fun/3);
-        'client.subscribe'    -> emqx:unhook(Hook, fun ?MODULE:Fun/3);
-        'client.unsubscribe'  -> emqx:unhook(Hook, fun ?MODULE:Fun/3);
-        'session.created'     -> emqx:unhook(Hook, fun ?MODULE:Fun/3);
-        'session.subscribed'  -> emqx:unhook(Hook, fun ?MODULE:Fun/4);
-        'session.unsubscribed'-> emqx:unhook(Hook, fun ?MODULE:Fun/4);
-        'session.terminated'  -> emqx:unhook(Hook, fun ?MODULE:Fun/3);
-        'message.publish'     -> emqx:unhook(Hook, fun ?MODULE:Fun/2);
-        'message.acked'       -> emqx:unhook(Hook, fun ?MODULE:Fun/3);
-        'message.deliver'     -> emqx:unhook(Hook, fun ?MODULE:Fun/3)
-    end.
+%% Called when the plugin application stop
+unload() ->
+    emqx:unhook('client.authenticate', fun ?MODULE:on_client_authenticate/2),
+    emqx:unhook('client.check_acl', fun ?MODULE:on_client_check_acl/5),
+    emqx:unhook('client.connected', fun ?MODULE:on_client_connected/4),
+    emqx:unhook('client.disconnected', fun ?MODULE:on_client_disconnected/3),
+    emqx:unhook('client.subscribe', fun ?MODULE:on_client_subscribe/4),
+    emqx:unhook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4),
+    emqx:unhook('session.created', fun ?MODULE:on_session_created/3),
+    emqx:unhook('session.resumed', fun ?MODULE:on_session_resumed/3),
+    emqx:unhook('session.subscribed', fun ?MODULE:on_session_subscribed/4),
+    emqx:unhook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4),
+    emqx:unhook('session.terminated', fun ?MODULE:on_session_terminated/3),
+    emqx:unhook('message.publish', fun ?MODULE:on_message_publish/2),
+    emqx:unhook('message.deliver', fun ?MODULE:on_message_deliver/3),
+    emqx:unhook('message.acked', fun ?MODULE:on_message_acked/3),
+    emqx:unhook('message.dropped', fun ?MODULE:on_message_dropped/3).
 
